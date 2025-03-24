d274f429c00288e99037d90eee564edeacfd9d3d
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-03-24 12:14:18 +0100

    updated-4 logger.js

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -3,36 +3,36 @@
     NODEJS EXPRESS | Flight API
 ------------------------------------------------------- */
 // $ npm i morgan
-// app.use(logger):
+// app.use(logger);
 
-// const morgan = require("morgan");
-// const fs = require("node:fs");
+const morgan = require("morgan");
+const fs = require("node:fs");
 
-// const now = new Date();
-// const today = now.toISOString().split("T")[0];
+const now = new Date();
+const today = now.toISOString().split("T")[0];
 
-// module.exports = morgan("combined", {
-//   stream: fs.createWriteStream(`./logs/${today}.log`, { flags: "a+" }),
-// });
+module.exports = morgan("combined", {
+  stream: fs.createWriteStream(`./logs/${today}.log`, { flags: "a+" }),
+});
 
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 
-const logDir = path.join(__dirname, "logs");
-const logFilePath = path.join(
-  logDir,
-  `${new Date().toISOString().slice(0, 10)}.log`
-);
+// const logDir = path.join(__dirname, "logs");
+// const logFilePath = path.join(
+//   logDir,
+//   `${new Date().toISOString().slice(0, 10)}.log`
+// );
 
-// Eğer 'logs' klasörü yoksa oluştur
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir, { recursive: true });
-}
+// // Eğer 'logs' klasörü yoksa oluştur
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir, { recursive: true });
+// }
 
-// Eğer günün log dosyası yoksa oluştur
-if (!fs.existsSync(logFilePath)) {
-  fs.writeFileSync(logFilePath, "");
-}
+// // Eğer günün log dosyası yoksa oluştur
+// if (!fs.existsSync(logFilePath)) {
+//   fs.writeFileSync(logFilePath, "");
+// }
 
-// Log dosyasına yazmak için kullanıyorsan:
-const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
+// // Log dosyasına yazmak için kullanıyorsan:
+// const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
